0e520ecd6830f7dde02fd3d04fd8bec3956b1b9d
Vingnir <vingnir@live.se>
2021-11-07 02:07:47 +0100

    added some demo components

---
+++ demo-app/src/App.js
@@ -0,0 +1,17 @@
+
+import Todo from './components/Todo';
+
+
+function App() {
+  return (
+    <div>
+      <h1> My Todos</h1>
+      <Todo text="Sign in" />
+      <Todo text="Sign up" />
+      <Todo text="Sign out" />
+      
+    </div>
+  );
+}
+
+export default App;
